ceece2ddfcfe258b317ca8b1f378c6f7e77b1cde
Martin Pichlmair <pi@brokenrul.es>
2023-10-04 11:51:14 +0200

    Adding varops

--- js/modifiers.js
+++ js/modifiers.js
@@ -3,165 +3,165 @@
  */
 
 
-    var isConsonant = function(c) {
-        c = c.toLowerCase();
-        switch(c) {
-            case 'a':
-                return false;
-            case 'e':
-                return false;
-            case 'i':
-                return false;
-            case 'o':
-                return false;
-            case 'u':
-                return false;
+var isConsonant = function (c) {
+    c = c.toLowerCase();
+    switch (c) {
+        case 'a':
+            return false;
+        case 'e':
+            return false;
+        case 'i':
+            return false;
+        case 'o':
+            return false;
+        case 'u':
+            return false;
 
-        }
-        return true;
-    };
+    }
+    return true;
+};
 
-    function endsWithConY(s) {
-        if (s.charAt(s.length - 1) === 'y') {
-            return isConsonant(s.charAt(s.length - 2));
-        }
-        return false;
-    };
+function endsWithConY(s) {
+    if (s.charAt(s.length - 1) === 'y') {
+        return isConsonant(s.charAt(s.length - 2));
+    }
+    return false;
+};
 
-    var modifiers = {
-        capitaliseAll : function(s) {
-            return s.replace(/(?:^|\s)\S/g, function(a) {
-                return a.toUpperCase();
-            });
+var modifiers = {
+    capitaliseAll: function (s) {
+        return s.replace(/(?:^|\s)\S/g, function (a) {
+            return a.toUpperCase();
+        });
 
-        },
+    },
 
-        capitalizeAll : function(s) {
-            return s.replace(/(?:^|\s)\S/g, function(a) {
-                return a.toUpperCase();
-            });
+    capitalizeAll: function (s) {
+        return s.replace(/(?:^|\s)\S/g, function (a) {
+            return a.toUpperCase();
+        });
 
-        },
+    },
 
-        caps : function(s) {
-            return s.charAt(0).toUpperCase() + s.slice(1);
+    caps: function (s) {
+        return s.charAt(0).toUpperCase() + s.slice(1);
 
-        },
+    },
 
-        capitalize : function(s) {
-            return s.charAt(0).toUpperCase() + s.slice(1);
+    capitalize: function (s) {
+        return s.charAt(0).toUpperCase() + s.slice(1);
 
-        },
+    },
 
-        inQuotes : function(s) {
-            return '"' + s + '"';
-        },
+    inQuotes: function (s) {
+        return '"' + s + '"';
+    },
 
-        comma : function(s) {
-            var last = s.charAt(s.length - 1);
-            if (last === ",")
-                return s;
-            if (last === ".")
-                return s;
-            if (last === "?")
-                return s;
-            if (last === "!")
-                return s;
-            return s + ",";
-        },
-
-        beeSpeak : function(s) {
-//            s = s.replace("s", "zzz");
-
-            s = s.replace(/s/, 'zzz');
+    comma: function (s) {
+        var last = s.charAt(s.length - 1);
+        if (last === ",")
             return s;
-        },
+        if (last === ".")
+            return s;
+        if (last === "?")
+            return s;
+        if (last === "!")
+            return s;
+        return s + ",";
+    },
 
-        a : function(s) {
-            if (!isConsonant(s.charAt()))
-                return "an " + s;
-            return "a " + s;
+    beeSpeak: function (s) {
+        //            s = s.replace("s", "zzz");
 
-        },
+        s = s.replace(/s/, 'zzz');
+        return s;
+    },
 
-        s : function(s) {
+    a: function (s) {
+        if (!isConsonant(s.charAt()))
+            return "an " + s;
+        return "a " + s;
 
-            var last = s.charAt(s.length - 1);
+    },
 
-            switch(last) {
-                case 'y':
+    s: function (s) {
 
-                    // rays, convoys
-                    if (!isConsonant(s.charAt(s.length - 2))) {
-                        return s + "s";
-                    }
-                    // harpies, cries
-                    else {
-                        return s.slice(0, s.length - 1) + "ies";
-                    }
-                    break;
+        var last = s.charAt(s.length - 1);
 
-                // oxen, boxen, foxen
-                case 'x':
-                    return s.slice(0, s.length - 1) + "en";
-                case 'z':
-                    return s.slice(0, s.length - 1) + "es";
-                case 'h':
-                    return s.slice(0, s.length - 1) + "es";
+        switch (last) {
+            case 'y':
 
-                default:
+                // rays, convoys
+                if (!isConsonant(s.charAt(s.length - 2))) {
                     return s + "s";
-            };
+                }
+                // harpies, cries
+                else {
+                    return s.slice(0, s.length - 1) + "ies";
+                }
+                break;
 
-        },
+            // oxen, boxen, foxen
+            case 'x':
+                return s.slice(0, s.length - 1) + "en";
+            case 'z':
+                return s.slice(0, s.length - 1) + "es";
+            case 'h':
+                return s.slice(0, s.length - 1) + "es";
 
-        n : function(s) {
-            return s + "\n";
-        },
+            default:
+                return s + "s";
+        };
 
-        title : function(s) {
-          return s.replace(
+    },
+
+    n: function (s) {
+        return s + "\n";
+    },
+
+    title: function (s) {
+        return s.replace(
             /\w\S*/g,
-            function(txt) {
-              return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            function (txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
             }
-          );
-        },
+        );
+    },
 
-        ed : function(s) {
+    ed: function (s) {
 
-            var index = s.indexOf(" ");
-            var s = s;
-            var rest = "";
-            if (index > 0) {
-                rest = s.substring(index, s.length);
-                s = s.substring(0, index);
+        var index = s.indexOf(" ");
+        var s = s;
+        var rest = "";
+        if (index > 0) {
+            rest = s.substring(index, s.length);
+            s = s.substring(0, index);
 
-            }
-
-            var last = s.charAt(s.length - 1);
-
-            switch(last) {
-                case 'y':
-
-                    // rays, convoys
-                    if (isConsonant(s.charAt(s.length - 2))) {
-                        return s.slice(0, s.length - 1) + "ied" + rest;
-
-                    }
-                    // harpies, cries
-                    else {
-                        return s + "ed" + rest;
-                    }
-                    break;
-                case 'e':
-                    return s + "d" + rest;
-
-                    break;
-
-                default:
-                    return s + "ed" + rest;
-            };
         }
-    };
+
+        var last = s.charAt(s.length - 1);
+
+        switch (last) {
+            case 'y':
+
+                // rays, convoys
+                if (isConsonant(s.charAt(s.length - 2))) {
+                    return s.slice(0, s.length - 1) + "ied" + rest;
+
+                }
+                // harpies, cries
+                else {
+                    return s + "ed" + rest;
+                }
+                break;
+            case 'e':
+                return s + "d" + rest;
+
+                break;
+
+            default:
+                return s + "ed" + rest;
+        };
+    }
+};
 
---
+++ js/utils.js
@@ -0,0 +1,29 @@
+
+function string2json(text) {
+	var rules = {}
+	var symbol = ""
+	var expansions = new Array()
+
+	let lines = text.split('\n')
+	lines.forEach(line => {
+		let trimmed = line.trim()
+
+		if (trimmed.length == 0 || trimmed.startsWith("\\\\"))
+			return
+
+		if (trimmed.startsWith("[") && trimmed.endsWith("]")) {
+			if (symbol.length > 0) {
+				rules[symbol] = expansions
+				expansions = new Array()
+			}
+			symbol = trimmed.substring(1, trimmed.length - 1)
+		} else {
+			expansions.push(trimmed)
+		}
+
+		if (symbol.length > 0) {
+			rules[symbol] = expansions
+		}
+	})
+	return rules
+}
