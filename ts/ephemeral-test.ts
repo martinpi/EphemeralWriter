0c14ad1a82123a694a8467596148b0fde572fbab
Martin Pichlmair <pi@brokenrul.es>
2023-10-18 12:19:22 +0200

    Added typescript support

--- ts/ephemeral-test.ts
+++
@@ -1,8 +0,0 @@
-import ephemeral from "ephemeral-writer";
-
-let text = "bla bla bla"
-
-let grammar = ephemeral.createGrammar(text)
-grammar.addModifiers(ephemeral.baseEngModifiers);
-let result1 = grammar.flatten("#origin#", false, { c: "Hello" })
-let result2 = grammar.expand("#origin#", false, { c: "Hello" })
